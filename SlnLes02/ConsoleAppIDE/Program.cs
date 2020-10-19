789e9f1b7b677ef5383daca0edf4b108b74400aa
joao1920 <jp.decarvalho@student.odisee.be>
2020-10-19 12:44:35 +0200

    Console+Wpf02

---
+++ SlnLes02/ConsoleAppIDE/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppIDE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" hallo World ");
+            Console.Write(" Hoe heet je: ");
+            string naam = Console.ReadLine();
+            Console.Write(" Aangenaam kennismaking, ");
+            Console.WriteLine(naam);
+            Console.WriteLine(" Druk op een knop om verder te gaan .. ");
+
+        }
+    }
+}
