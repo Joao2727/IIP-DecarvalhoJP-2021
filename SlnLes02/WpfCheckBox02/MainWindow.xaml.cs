4b7ad52176fccfa3a94be6234e8817cd23a62c91
joao1920 <jp.decarvalho@student.odisee.be>
2020-10-19 16:39:45 +0200

    16U update

---
+++ SlnLes02/WpfCheckBox02/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCheckBox02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChckBxYsNo_Checked(object sender, RoutedEventArgs e)
+        {
+            LblText.Content = "Ja, ik ben er mee eens";
+        }
+
+        private void ChckBxYsNo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            LblText.Content = "Nee, niet mee eens";
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (ChckBxYsNo2.IsChecked == true) {
+                LblText1.Content = "Ja, ik doe mee met online les";
+            } else
+            {
+
+                LblText1.Content = "Neen, ik doe niet mee met online les";
+            }                
+        }
+    }
+}
