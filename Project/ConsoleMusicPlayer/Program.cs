4d0ac4fb2009c930baeaa406d1d8ff9114eda17e
joao1920 <jp.decarvalho@student.odisee.be>
2020-10-25 21:30:25 +0100

    ProjectFase1

---
+++ Project/ConsoleMusicPlayer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using WMPLib;
+
+
+namespace ConsoleMusicPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowsMediaPlayer musicplayer = new WindowsMediaPlayer();
+
+            Console.WriteLine(" MEDIAPLAYER ");
+            Console.WriteLine(" =========== ");
+
+             Console.WriteLine($"Bestand afspelen : C:\\Users\\joao-\\mymusic\\Te-Amo.mp3");
+            
+            string musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            musicplayer.URL = System.IO.Path.Combine(musicFolder, @"C:\\Users\\joao-\\mymusic\\Te-Amo.mp3");
+            //Het liedje wordt gestart wanneer de console wordt opengemaakt.
+            musicplayer.controls.play();
+            Console.ReadLine();
+            //Het liedje wordt gepauzeerd
+            //musicplayer.controls.play();
+            //
+            musicplayer.controls.stop();
+
+            int a = 1;
+            if (a == 1)
+            {
+                musicplayer.controls.stop();
+            }
+
+
+        }
+    }
+}
